9c4fab9ac6816ee2d6b3970f2ec2cfa2365a8203
Alvinn500 <alfhin1212@gmail.com>
2024-12-31 15:09:01 +0700

    fix: fixing ui student dashboard

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-student-layout title="Dashboard">
-    <div class="md:flex grid grid-rows-2 gap-8 mb-0 md:mb-8 mt-14 md:mt-0">
-        <div class="flex mx-auto md:mx-0 dark-purple rounded-xl w-full md:w-3/5 md:min-w-[298.73px] duration-600 ease-in-out">
+    <div class="grid grid-rows-2  md:flex  gap-8 mb-0 md:mb-8 mt-14 md:mt-0">
+        <div class="flex mx-auto md:mx-0 dark-purple rounded-xl w-full md:w-[70%] lg:w-3/5 md:min-w-[298.73px] duration-600 ease-in-out">
             <div class="w-3/5">
                 <div class="px-5 pt-5 mb-3">
                     <h3 class="uppercase text-xl font-medium">student</h3>
@@ -27,33 +27,35 @@
                     <img class="w-[16%] md:w-[14%] aspect-square" src="{{asset("img/badge/5-gs.png")}}" alt="badge">
                 </div>
             </div>
-            <div class="w-2/5">
-                <img class="w-full -mt-10 mx-auto" src="{{asset("img/character/2.png")}}" alt="character">
+            <div class="w-[30%] lg:w-2/5">
+                <img class="w-full -mt-10 mx-auto" src="{{asset("img/character/0.png")}}" alt="character">
             </div>
             
         </div>
-        <div class="flex md:flex-wrap justify-center lg:justify-normal gap-4 md:gap-6 w-full md:w-2/5">
-            <div class="dark-green p-5 rounded-xl md:min-w-[165px] w-1/3 h-32">
-                <h3 class="text-sm md:text-base uppercase mb-4 md:mb-2">total xp</h3>
-                <p class="text-2xl md:text-3xl font-medium text-center">500</p>
-            </div>
-            <div class="dark-green p-5 rounded-xl md:min-w-[165px] w-1/3 h-32">
-                <h3 class="text-sm md:text-base uppercase mb-4 md:mb-2">level</h3>
-                <p class="text-2xl md:text-3xl font-medium text-center">1</p>
-            </div>
-            <div class="mx-auto md:mx-0 bg-main p-2 md:p-4 rounded-xl md:min-w-[165px] w-1/3 h-32">
-                <div class="gradient-diskusi w-full h-full rounded-lg pt-6 pl-3">
-                    <h3 class="text-sm md:text-base mb-2 md:mb-4 font-semibold">Forum Diskusi</h3>
-                    <p class="text-xs md:text-sm font-normal md:font-medium">Jelajahi Forum</p>
+        <div class="h-1/2">
+            <div class="flex md:flex-col lg:flex-row lg:flex-wrap justify-center lg:justify-normal gap-4 md:gap-6 w-full ">
+                <div class="dark-green p-5 rounded-xl sm:min-w-[180px] lg:min-w-[140px] xl:min-w-[180px] w-1/3 h-32">
+                    <h3 class="text-sm md:text-base uppercase mb-4 md:mb-2">total xp</h3>
+                    <p class="text-2xl md:text-3xl font-medium text-center">500</p>
+                </div>
+                <div class="dark-green p-5 rounded-xl sm:min-w-[180px] lg:min-w-[140px] xl:min-w-[180px] w-1/3 h-32">
+                    <h3 class="text-sm md:text-base uppercase mb-4 md:mb-2">level</h3>
+                    <p class="text-2xl md:text-3xl font-medium text-center">1</p>
+                </div>
+                <div class="mx-auto md:mx-0 bg-main p-2 md:p-4 rounded-xl sm:min-w-[180px] lg:min-w-[140px] xl:min-w-[180px] w-1/3 h-32">
+                    <div class="gradient-diskusi w-full h-full rounded-lg pt-6 pl-3">
+                        <h3 class="text-sm md:text-base mb-2 md:mb-4 font-semibold">Forum Diskusi</h3>
+                        <p class="text-xs md:text-sm font-normal md:font-medium">Jelajahi Forum</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="-mt-32 md:-mt-0">
+    <div class="-mt-32 sm:-mt-40 md:mt-0 ">
         <h3 class="font-semibold mb-4">Kelas Terbaru</h3>
         <div class="mb-4">
-            <div class="flex bg-main p-4 rounded-lg items-center gap-4 w-full sm:w-1/2 md:w-1/3">
-                <img class="ratio-16x9 h-10 rounded-md" src="{{asset("img/luluu.jpg")}}" alt="class image">
+            <div class="flex bg-main p-4 rounded-lg items-center gap-4 w-full sm:w-3/5 md:w-1/3">
+                <img class="ratio-16x9 h-10 rounded-md" src="{{asset("img/default.jpg")}}" alt="class image">
                 <h3>kelas</h3>
             </div>
         </div> 
