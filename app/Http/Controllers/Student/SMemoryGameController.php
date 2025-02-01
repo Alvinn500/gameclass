5c96f96767efc400f14266b5da0be75af1a5f9c0
Alvinn500 <alfhin1212@gmail.com>
2025-02-01 22:11:29 +0700

    fix: validate game

--- app/Http/Controllers/Student/SMemoryGameController.php
+++ app/Http/Controllers/Student/SMemoryGameController.php
@@ -25,7 +25,7 @@ class SMemoryGameController extends Controller
         if($game) {
             $gameImages = Arr::shuffle($game->images);
         }
-        
+        // dd($game->answers->where('user_id', Auth::user()->id)->count());
         $breadcrumbs = [
             ['link' => "/student/class/", 'name' => 'Kelas' ],
             ['link' => "/student/class/{$class->id}", 'name' => $class->study_name],
